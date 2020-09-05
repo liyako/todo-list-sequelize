e1d80d9358885ed960417c12377cda445ecb92fc
cotool <tusky.saga@gmail.com>
2020-09-06 05:18:00 +0800

    [新增]flash and message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 
 
@@ -29,6 +30,15 @@ app.use(session({
 }))
 
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
 // route setting
 app.use(routes)
 
