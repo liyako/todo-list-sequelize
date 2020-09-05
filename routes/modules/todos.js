aeda9853fa1fb488dc55d9060264466c05ea10f6
cotool <tusky.saga@gmail.com>
2020-09-06 05:31:56 +0800

    [新增]edit function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
-const User = db.User
+//const User = db.User
 
 //new
 router.get('/new', (req, res) => {
@@ -24,8 +24,34 @@ router.get('/:id', (req, res) => {
   return Todo.findOne({
     where: { id, UserId }
   })
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+  .then(todo => res.render('detail', { todo: todo.toJSON() }))
+  .catch(error => console.log(error))
+})
+//edit
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.get() }))
     .catch(error => console.log(error))
-  })
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  console.log(req.body)
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
 
 module.exports = router
