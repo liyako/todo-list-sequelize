8b3d7c3fec15527fa807e5608f9304cce89b6360
cotool <tusky.saga@gmail.com>
2020-09-06 04:38:23 +0800

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+
+
+
+
+const routes = require('./routes')
+
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
+// route setting
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
