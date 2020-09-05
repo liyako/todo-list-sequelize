cf1fa7732082a8fc8129424364a4cc066febe23c
cotool <tusky.saga@gmail.com>
2020-09-06 05:08:37 +0800

    [新增]authentication todo and home router

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator,home)
 
 
 module.exports = router
